d144a8a27655fe9dc2177b1af97000bda3960af1
Aditya Bhardwaj <aditya_bhardwaj.scsebtech@galgotiasuniversity.edu.in>
2023-03-31 16:01:29 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import Main from './components/Main';
+import Tutorial from './components/Tutorial';
+import './App.css';
+
+function App() {
+  const [showTutorial, setShowTutorial] = useState(false);
+  return <div className='App'>{showTutorial ? <Tutorial close={() => setShowTutorial(false)} /> : <Main />}</div>;
+}
+
+export default App;
